4de198fd5f7162524b6210db6e1c21060941ff78
Vinicius <visacastelani@gmail.com>
2018-08-22 11:50:04 -0300

    Adicionado funcionalidades JS

---
+++ js/script.js
@@ -0,0 +1,107 @@
+/*       FASE 1              */
+var start = document.getElementById('start');
+var step = document.getElementById('step');
+/*       FASE 1              */
+
+/*       FASE 2              */
+var step1 = document.getElementById('step1');
+var next1 = document.getElementById('next1');
+/*       FASE 2              */
+
+/*       FASE 3              */
+var step2 = document.getElementById('step2');
+var next2 = document.getElementById('next2');
+/*       FASE 3              */
+
+/*       FASE 4              */
+var step3 = document.getElementById('step3');
+var next3 = document.getElementById('next3');
+/*       FASE 4              */
+
+
+/*       FASE 5              */
+var step4 = document.getElementById('step4');
+var next4 = document.getElementById('next4');
+/*       FASE 5              */
+
+/*       FASE  6             */
+var step5 = document.getElementById('step5');
+var next5 = document.getElementById('step5');
+/*       FASE  6             */
+
+/*       FASE  7             */
+var step6 = document.getElementById('step6');
+var next6 = document.getElementById('next6');
+/*       FASE  7             */
+
+/*       FASE  8             */
+var step7 = document.getElementById('step7');
+var next7 = document.getElementById('next7');
+/*       FASE  8            */
+
+/*       FASE  8             */
+var step8 = document.getElementById('step8');
+var next8 = document.getElementById('next8');
+/*       FASE  8            */
+
+/*       FASE  9             */
+var step9 = document.getElementById('step9');
+var next9 = document.getElementById('next9');
+/*       FASE  9            */
+
+/*       FASE  10             */
+var step10 = document.getElementById('step10');
+var next10 = document.getElementById('next10');
+/*       FASE  10            */
+
+
+start.onclick = function(){
+	step.style.display = "none";
+	step1.style.display = "block";
+};
+
+next1.onclick = function(){
+	step1.style.display = "none";
+	step2.style.display = "block";
+}
+
+next2.onclick = function(){
+	step2.style.display = "none";
+	step3.style.display = "block";
+}
+
+next3.onclick = function(){
+	step3.style.display = "none";
+	step4.style.display = "block";
+}
+
+next4.onclick = function(){
+	step4.style.display = "none";
+	step5.style.display = "block";
+}
+
+next5.onclick = function(){
+	step5.style.display = "none";
+	step6.style.display = "block";
+}
+
+next6.onclick = function(){
+	step6.style.display = "none";
+	step7.style.display = "block";
+}
+
+next7.onclick = function(){
+	step7.style.display = "none";
+	step8.style.display = "block";
+}
+
+next8.onclick = function(){
+	step8.style.display = "none";
+	step9.style.display = "block";
+}
+
+next9.onclick = function(){
+	step9.style.display = "none";
+	step10.style.display = "block";
+}
+
